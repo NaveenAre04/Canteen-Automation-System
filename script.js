5e1aff602cc9aa8f8749a9590498aa54b9399e36
NaveenAre04 <90562385+NaveenAre04@users.noreply.github.com>
2021-11-18 20:48:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+function add1() {
+
+    var value = parseInt(document.getElementById('result1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result1').value = value;
+    
+}
+function sub1() {
+
+    var value = parseInt(document.getElementById('result1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result1').value = value;
+    
+}
+function add2() {
+
+    var value = parseInt(document.getElementById('result2').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result2').value = value;
+    
+}
+function sub2() {
+
+    var value = parseInt(document.getElementById('result2').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result2').value = value;
+    
+}
+function add3() {
+
+    var value = parseInt(document.getElementById('result3').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result3').value = value;
+    
+}
+function sub3() {
+
+    var value = parseInt(document.getElementById('result3').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result3').value = value;
+    
+}
+function add4() {
+
+    var value = parseInt(document.getElementById('result4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result4').value = value;
+    
+}
+function sub4() {
+
+    var value = parseInt(document.getElementById('result4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result4').value = value;
+    
+}
+function add5() {
+
+    var value = parseInt(document.getElementById('result5').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result5').value = value;
+    
+}
+function sub5() {
+
+    var value = parseInt(document.getElementById('result5').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result5').value = value;
+    
+}
+function add6() {
+
+    var value = parseInt(document.getElementById('result6').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result6').value = value;
+    
+}
+function sub6() {
+
+    var value = parseInt(document.getElementById('result6').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result6').value = value;
+    
+}
+function add7() {
+
+    var value = parseInt(document.getElementById('result7').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result7').value = value;
+    
+}
+function sub7() {
+
+    var value = parseInt(document.getElementById('result7').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result7').value = value;
+    
+}
+function add8() {
+
+    var value = parseInt(document.getElementById('result8').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result8').value = value;
+    
+}
+function sub8() {
+
+    var value = parseInt(document.getElementById('result8').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result8').value = value;
+    
+}
+function add9() {
+
+    var value = parseInt(document.getElementById('result9').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('result9').value = value;
+    
+}
+function sub9() {
+
+    var value = parseInt(document.getElementById('result9').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('result9').value = value;
+    
+}
